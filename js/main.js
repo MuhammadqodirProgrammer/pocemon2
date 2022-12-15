73c2bb7d513b0c692adbf1cec6340ba65ffc0666
MuhammadqodirProgrammer <komilovm831@gmail.com>
2022-12-16 00:11:00 +0500

    last commit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const selectEl = document.querySelector("#select");
 function domgachiqarator(array, node) {
   let cards = "";
   for (item of array) {
-    cards += `  <div class="card col-lg-3 col-md-4 col-sm-12 col-10 mx-sm-auto  mx-3 text-center  shadow-lg p-2  rounded  rounded-4  mt-2 mb-4 " >
+    cards += `  <div class="card col-lg-3 col-md-4 col-sm-12 col-10 mx-sm-auto hvr-grow mx-3 text-center  shadow-lg p-2  rounded  rounded-4  mt-2 mb-4 " >
      <img src="${item.img}" class="card-img-top" alt="images">
   <div class="card-body">
   <h4 class="card-title">${item.id}</h4>
