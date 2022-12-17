1ff6535f607e2a3947f1a0ebb41d3f5b47a10e66
MuhammadqodirProgrammer <komilovm831@gmail.com>
2022-12-17 18:03:39 +0500

    search

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+
 const cardel = document.querySelector("#card-box");
 const selectEl = document.querySelector("#select");
+const inputEl =document.querySelector("input")
+const formEl =document.querySelector("form")
 
 function domgachiqarator(array, node) {
   let cards = "";
@@ -54,3 +57,63 @@ for(i of filled){
   options.textContent = i;
   options.setAttribute("value",i)
 }
+
+let newarr2= []
+
+formEl.addEventListener("input", (evt) =>{
+  evt.preventDefault()
+ cardel.innerHTML =""
+  let elinputval =inputEl.value.toLocaleLowerCase();
+
+pokemons.forEach((el) => {
+  if(el.name.toLocaleLowerCase().includes(elinputval)){
+    newarr2.push(el)
+  }
+});
+domgachiqarator(newarr2,cardel)
+newarr2 =[]
+})
+
+console.log(newarr2)
+
+
+
+
+
+// const cardel =document.querySelector("#card-box")
+// const inputEl =document.querySelector("input")
+// const formEl =document.querySelector("input")
+
+// let newarr= []
+
+// formEl.addEventListener("input", (evt) =>{
+//   evt.preventDefault()
+
+//   let elinputval =inputEl.value;
+
+// pokemons.forEach((el) => {
+//   if(el.name.includes(elinputval)){
+//     newarr.push(el)
+//   }
+// });
+// })
+// function myFunc() {
+//     let cards =""
+//     for( item of pokemons){
+//          cards +=`  <div class="card col-lg-3 col-md-4 col-sm-12 col-12 mx-sm-auto  mx-3 text-center  shadow-lg p-2  rounded  rounded-4  mt-2 mb-4 " >
+//           <img src="${item.img}" class="card-img-top" alt="images">
+//        <div class="card-body">
+//        <h4 class="card-title">${item.id}</h4>
+//         <h5 class="card-title">${item.name}</h5>
+//         <h5 class="card-title">${item.spawn_time}</h5>
+//         <p class="card-text"><span> height: ${item.height}</span><br><span> weight: ${item.weight}</span></p>
+//         <h6 class="card-title"><span>${item.weaknesses}</span></h6>
+//       </div>
+//       </div> `
+   
+//       console.log(item.egg);
+//     }
+    
+//     cardel.innerHTML =cards
+// }
+// myFunc()
